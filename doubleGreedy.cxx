3656423cb837b97affe08ecd014927489078d105
Gustav Ewing <gustavew@chalmers.se>
2025-03-31 12:50:41 +0200

    WIP version of double that fails to compile

--- doubleGreedy.cxx
+++ doubleGreedy.cxx
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <string>
 #include <sys/types.h>
+#include <tuple>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -14,7 +15,9 @@ using namespace std;
 
 using Weight = uint32_t;
 // using Graph = std::unordered_map<std::pair<int, int>, Weight>;
-using Pairing = vector<tuple<uint32_t, uint32_t, uint32_t>>;
+
+using Pair = tuple<uint32_t, uint32_t, uint32_t>;
+using Pairing = vector<Pair>;
 
 struct pair_equal {
   bool operator()(const pair<int, int> &lhs, const pair<int, int> &rhs) const {
@@ -40,6 +43,8 @@ const static auto make_edge = [](int a, int b) {
 
 using Graph = unordered_map<std::pair<int, int>, Weight, pair_hash, pair_equal>;
 Pairing doubleGreedy(Graph graph);
+
+u_int32_t nextEdge(u_int32_t node, vector<u_int32_t> path, bool dist[]);
 
 int main() {
   string filename = "graph.txt";
@@ -68,12 +73,37 @@ int main() {
   Pairing result;
   result = doubleGreedy(graph);
 
+  /*
+  //testcode for the print below
+  Pair test = make_tuple(1, 2, 10);
+  result.clear();
+  result.push_back(test);
+  */
+
+  int counter = 0;
+  for (Pair element : result) {
+    cout << "Pair " << counter++ << " :" << "\n";
+    cout << "\t" << "First Node: " << "\t" << get<0>(element) << "\n";
+    cout << "\t" << "Second Node: " << "\t" << get<1>(element) << "\n";
+    cout << "\t" << "Weight: " << "\t" << get<2>(element) << "\n";
+  }
+
   return 0;
 }
 
 Pairing doubleGreedy(Graph graph) {
   Pairing matching;
   const u_int32_t size = 10; // manually set for now
+
+  bool dist[size];
+  int counter = 0;
+  // true here implies producer
+  for (bool &entry : producer) {
+    if (counter++ % 3 == 0) {
+      entry = true;
+    }
+  }
+
   bool producer[size];
   for (bool &entry : producer) {
     entry = true;
@@ -88,16 +118,31 @@ Pairing doubleGreedy(Graph graph) {
     vector<u_int32_t> path; // init path
 
     // find available node
-    if (producer[i] != true) {
+    if (!producer[i] || !dist[i]) {
       continue;
     }
 
     path.push_back(i); // add to path
 
-    // todo repeat for rest of path
+    // repeat for rest of path
+    u_int32_t nextNode = i;
+    u_int32_t originNode;
+    // fix this != 0 condition so it ends the loop properly
+    while (nextNode != 0) {
+      originNode = nextNode;
+      nextNode = nextEdge(originNode, path, dist);
+      path.push_back(nextNode);
+    }
   }
 
   return matching;
+}
+
+u_int32_t nextEdge(u_int32_t node, vector<u_int32_t> path, bool dist[]) {
+  bool producer = false;
+  if (dist[node]) {
+    producer = true;
+  }
 }
 
 // to test whether the graph was read in correctly
@@ -109,4 +154,6 @@ int test(Graph graph) {
     stream << key.first << " " << key.second << " " << value << '\n';
   }
   stream.close();
+
+  return 0;
 }
